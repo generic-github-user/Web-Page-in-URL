4efe7c48906c4ac689606428210fe920c30752ef
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-05 17:02:28 -0500

    Add function to get variable/parameter from URL string getQueryVariable()
    https://css-tricks.com/snippets/javascript/get-url-variables/

--- src/script.js
+++ src/script.js
@@ -1,5 +1,18 @@
 var wl = window.location;
 
+// https://css-tricks.com/snippets/javascript/get-url-variables/
+function getQueryVariable(variable) {
+      var query = window.location.search.substring(1);
+      var vars = query.split("&");
+      for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) {
+                  return pair[1];
+            }
+      }
+      return (false);
+}
+
 function encode(input) {
       wl.href = wl.origin + wl.pathname + "?content=" + encodeURI(input);
 }
