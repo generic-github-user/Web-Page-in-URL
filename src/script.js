7ca86f2ac05aad3998f7afe93fa99318829d492a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-05 19:56:20 -0500

    Replace default getQueryVariable function with function specifically for content URL variable
    See bug #2

--- src/script.js
+++ src/script.js
@@ -2,27 +2,20 @@ var wl = window.location;
 var input_box = document.querySelector("#input-box");
 input_box.setAttribute("oninput", "encode();");
 
-// https://css-tricks.com/snippets/javascript/get-url-variables/
-function getQueryVariable(variable) {
-      var query = window.location.search.substring(1);
-      var vars = query.split("&");
-      for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split("=");
-            if (pair[0] == variable) {
-                  return pair[1];
-            }
-      }
-      return (false);
+function getUrlVariable(variable) {
+      var path = wl.origin + wl.pathname + "?content=";
+      var length = path.length;
+      return wl.href.slice(length);
 }
 
 function encode() {
       var input = input_box.value;
       var path = wl.pathname + "?content=" + escape(input);
       window.history.pushState("", "HTMLURL", path);
-      document.querySelector("#content-div").innerHTML = unescape(getQueryVariable("content"));
+      document.querySelector("#content-div").innerHTML = unescape(getUrlVariable("content"));
 }
 
-var content = getQueryVariable("content");
+var content = getUrlVariable("content");
 if (content == undefined || content == "") {
       input_box.value = def;
       encode();
